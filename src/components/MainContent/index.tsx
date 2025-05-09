06d2c7da976293b3e7cc4c2990f5f97f3d6ee28f
NattanSilva <natanxboy@gmail.com>
2025-05-09 17:58:23 -0300

    Add: main content base structure

---
+++ src/components/MainContent/index.tsx
@@ -0,0 +1,7 @@
+export function MainContent() {
+  return (
+    <main className='w-full max-w-[1240px] h-[calc(100vh-7rem)] flex items-center justify-center'>
+      <div className='w-[85%]'>Content</div>
+    </main>
+  )
+}
